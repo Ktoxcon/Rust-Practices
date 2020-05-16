69b6eb079a1dc6f0c8ee8b30055790cdae2d4ce2
Ktoxcon <kennethtoxro@gmail.com>
2020-05-16 17:58:46 -0600

    1.0.0

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
 fn main() {
-    println!("Hello, world!");
+    // PRACTICING VARIABLES IN RUST
+
+    // immutable variable, the underscore indicates that this are unused variables
+    let _x = 5;
+    let _y = 6;
+
+    // this throws an error, because x and y are immutable variables
+    // x = 7;
+
+    // adding mut to the beginning of a variable declaration and making it mutable
+    let mut mutable = 45;
+    println!("{}",mutable);
+    mutable = 46;
+    println!("{}",mutable);
+
+    // some constants
+    const CONSTANT:f32 = 3.14;
+    println!("{}",CONSTANT);
+
+    // shadowing variables
+    let shadowed = "   ";
+    let shadowed = shadowed.len();
+    println!("{}",shadowed);
+
 }
--- src/variables.rs
+++ src/variables.rs
@@ -1,3 +1,9 @@
 // PRACTICING VARIABLES IN RUST
 
+// immutable variable
+let x = 5;
+let y = 6;
+
+// this throws an error, cause x and y are immutable variables
+let x = y;
 
