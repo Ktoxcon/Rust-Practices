71141b94ca704468bd1d9e46dc6839723fd99b33
Ktoxcon <kennethtoxro@gmail.com>
2020-05-16 18:34:46 -0600

    commit

--- src/main.rs
+++ src/main.rs
@@ -23,4 +23,22 @@ fn main() {
     let shadowed = shadowed.len();
     println!("{}",shadowed);
 
+    // UNSIGNED INTEGER
+    let eight:u8 = 255;
+    let six_teen:u16 = 1000;
+    let tirty_two:u32 = 32637;
+    let sixty_four:u64 = 9546678;
+    let one_two_eight:u128 = 12345678908934567;
+    //throws an error, an unsigned int cant be negative
+    //let error:u16 = -3;
+    println!("{},{},{},{},{}",eight,six_teen,tirty_two,sixty_four,one_two_eight);
+
+
+    //SIGNED INT
+    let mut signed_int:i16 = -30;
+    println!("{}",signed_int);
+    signed_int = 90;
+    println!("{}",signed_int);
+    let signed_int = signed_int -34;
+    println!("{}",signed_int);
 }
