70fb7b48165123dff2b79ab27bb62fc3b43bab6e
Ktoxcon <kennethtoxro@gmail.com>
2020-05-19 09:00:15 -0600

    LOOPS

--- src/loops.rs
+++ src/loops.rs
@@ -20,3 +20,16 @@ fn loops_l() {
         }
     }
 }
+
+fn while_loops() {
+    let mut desc = 10;
+    while desc != 0 {
+        println!("{:*<count$}"," ",count = desc);
+        desc -= 1;
+    }
+    let mut asc = 0;
+    while asc != 10 {
+        println!("{}{:*>count$}","#"," ",count = asc);
+        asc += 1;
+    }
+}
