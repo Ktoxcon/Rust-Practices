ef2bf0d87ccba7dd22ad8fd40f23184aa7cdc847
Ktoxcon <kennethtoxro@gmail.com>
2020-05-21 12:48:52 -0600

    FINAL

--- src/calculator/calculator/src/main.rs
+++ src/calculator/calculator/src/main.rs
@@ -6,9 +6,6 @@ fn main() {
 
 fn menu(){
 
-
-    let options = [1,2,3,4];
-
     println!("{:*>25}","");
     println!("{: >7}{}"," ","CALCULATOR");
     println!("{:*>25}","");
@@ -23,32 +20,63 @@ fn menu(){
        .read_line(&mut option)
        .expect("Input error");
 
-    let option:u32 = match option.trim().parse() {
+    let election = match option.trim().parse() {
         Ok(num) => num,
         Err(_) => 0,
     };
 
+    let  x = String::new();
+    let  y = String::new();
 
-    let mut x = String::new();
-    let mut y = String::new();
-
-    for option in 1..4 {
+    operations(election,x,y);
         
-        if option == 1 {
-            println!("GIVE ME TWO NUMBERS");
-            println!("THE SUM IS:{}",result);
-            break;
-        }
-
-    }
     
+    
+}
 
 
+fn operations(e:i32,x:String,y:String) {
+    if e == 1 {
+        println!("GIVE ME TWO NUMBERS:");
+        println!("THE SUM IS:{}",sum(conv(x),conv(y)));
+    }else if e == 2 {
+        println!("GIVE ME TWO NUMBERS:");
+        println!("THE SUB IS:{}",sub(conv(x),conv(y)));
+    }else if e == 3 {
+        println!("GIVE ME TWO NUMBERS:");
+        println!("THE MUL IS:{}",mul(conv(x),conv(y)));
+    }else if e == 4 {
+        println!("GIVE ME TWO NUMBERS:");
+        println!("THE DIV IS:{}",div(conv_float(x),conv_float(y)));
+    }else {
+        menu();
+    }
+}
 
 fn conv(mut num:String) -> i32 {
 
     io::stdin().read_line(&mut num).expect("Err");
-    
+    let num:i32 = match num.trim().parse() {
+        Ok(num) => num,
+        Err(_) => 0,
+    };
+
+    num
+}
+
+fn conv_float(mut num:String) -> f32 {
+
+    io::stdin().read_line(&mut num).expect("Err");
+    let num:f32 = match num.trim().parse() {
+        Ok(num) => num,
+        Err(_) => 0.0,
+    };
+
+    num
+}
+
+fn sum(x:i32,y:i32) -> i32 {
+    x + y
 }
 
 fn sub(x:i32,y:i32) -> i32 {
@@ -59,12 +87,12 @@ fn mul(x:i32,y:i32) -> i32 {
     x * y
 }
 
-fn div(x:i32,y:i32) -> i32 {
-    if y == 0 {
+fn div(x:f32,y:f32) -> f32 {
+    if y == 0 as f32 {
         println!("Div by Zero!");
-        y
+        0.0
     }else {
-        x / y
+       x / y
     }
 
 }
