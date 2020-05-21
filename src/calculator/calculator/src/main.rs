736bc28748f66bbf274687148adddabc3bd41cf3
Ktoxcon <kennethtoxro@gmail.com>
2020-05-21 12:00:51 -0600

    CALC 1.0

--- src/calculator/calculator/src/main.rs
+++ src/calculator/calculator/src/main.rs
@@ -5,6 +5,10 @@ fn main() {
 }
 
 fn menu(){
+
+
+    let options = [1,2,3,4];
+
     println!("{:*>25}","");
     println!("{: >7}{}"," ","CALCULATOR");
     println!("{:*>25}","");
@@ -24,18 +28,27 @@ fn menu(){
         Err(_) => 0,
     };
 
-    for option in (1..4) {
+
+    let mut x = String::new();
+    let mut y = String::new();
+
+    for option in 1..4 {
+        
         if option == 1 {
-            println!("MATCHED WITH 1");
+            println!("GIVE ME TWO NUMBERS");
+            println!("THE SUM IS:{}",result);
+            break;
         }
 
     }
     
 
-}
 
-fn sum(x:i32,y:i32) -> i32 {
-    x + y
+
+fn conv(mut num:String) -> i32 {
+
+    io::stdin().read_line(&mut num).expect("Err");
+    
 }
 
 fn sub(x:i32,y:i32) -> i32 {
